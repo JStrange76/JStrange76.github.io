fbdacf117bb55e890c8abe45ef2b800587adccbd
JStrange76 <bethanyjo.strange@gmail.com>
2024-10-26 10:43:56 -0600

    Intial upload of portfolio
    Tech Mom's assignment - uploaded html, css, and javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+// const greeting = document.getElementById("greeting");
+
+// // greeting.style.textAlign = "center";
+// // greeting.style.marginTop = "50px";
+// // // greeting.style.fontFamily = "Arial, sans-serif"; // Change font type
+// // greeting.style.fontSize = "24px"; // Change font size
+// // greeting.style.color = "blue"; // Change font color
+
+// // const username = window.prompt("What's your name?", "Jane");
+// // greeting.innerHTML = "Welcome to my Website, " + username + "!";
